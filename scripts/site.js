d20a5b6e492b569ce3743412d28e2da6af410520
yuxuanchenn <yuxuanc1212@gmail.com>
2020-01-27 00:02:00 -0500

    updated site

--- scripts/site.js
+++ scripts/site.js
@@ -1,13 +1,13 @@
-$(document).bind("contextmenu",function(e) {
-	e.preventDefault();
-
-});
-
-$(document).keydown(function(e){
-    if(e.which === 123){
-
-       return false;
-
-    }
-
-});
+// $(document).bind("contextmenu",function(e) {
+// 	e.preventDefault();
+//
+// });
+//
+// $(document).keydown(function(e){
+//     if(e.which === 123){
+//
+//        return false;
+//
+//     }
+//
+// });
